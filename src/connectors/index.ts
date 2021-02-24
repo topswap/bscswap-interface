37b4aa7f3e242e5b46388a0d54dacbb234c0c321
top1st <riguang102@gmail.com>
2021-02-24 12:29:02 +0800

    Update index.ts

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -29,7 +29,7 @@ export const binanceinjected = new BinanceConnector({
 
 // mainnet only
 export const walletconnect = new WalletConnectConnector({
-  rpc: { 56: NETWORK_URL },
+  rpc: { 1: NETWORK_URL },
   bridge: 'https://bridge.walletconnect.org',
   qrcode: true,
   pollingInterval: 15000
