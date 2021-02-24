5f35743c8d50a6b16e18a6dce121a5df901a5e38
top1st <riguang102@gmail.com>
2021-02-24 12:22:41 +0800

    Update index.ts

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -29,7 +29,7 @@ export const binanceinjected = new BinanceConnector({
 
 // mainnet only
 export const walletconnect = new WalletConnectConnector({
-  rpc: { 1: NETWORK_URL },
+  rpc: { 56: NETWORK_URL },
   bridge: 'https://bridge.walletconnect.org',
   qrcode: true,
   pollingInterval: 15000
@@ -38,13 +38,13 @@ export const walletconnect = new WalletConnectConnector({
 // mainnet only
 export const fortmatic = new FortmaticConnector({
   apiKey: FORMATIC_KEY ?? '',
-  chainId: 1
+  chainId: 56
 })
 
 // mainnet only
 export const portis = new PortisConnector({
   dAppId: PORTIS_ID ?? '',
-  networks: [1]
+  networks: [56]
 })
 
 // mainnet only
